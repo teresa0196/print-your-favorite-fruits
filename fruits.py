bbe284247e4fd0c922e953c718c4372d2e6eb9d7
Teresa Jeon <teresajeon@seoulchristian.org>
2017-03-28 23:00:56 +0900

    Add fruits.py

---
+++ fruits.py
@@ -0,0 +1,3 @@
+print "mango\npeach" # print 'mango' and 'peach' in different lines
+print "tangerine\nmangosteen" # print 'tangerine' and 'mangosteen' in different lines
+print "melon" # print 'melon'
